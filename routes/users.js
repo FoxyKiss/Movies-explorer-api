3a64e12cc21b5ed3193f43cd82488f0d90245898
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-05-25 19:08:41 +0600

    fix: Исправить валидацию для Email

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const isEmail = require('validator/lib/isEmail');
 const { getCurrentUser, updateUserInfo } = require('../controllers/users');
 
 router.get('/me', getCurrentUser);
@@ -8,7 +7,7 @@ router.get('/me', getCurrentUser);
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    email: Joi.string().required().custom(isEmail),
+    email: Joi.string().required().email(),
   }),
 }), updateUserInfo);
 
